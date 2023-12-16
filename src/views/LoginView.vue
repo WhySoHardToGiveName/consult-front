0e2ed2a8db4b8c47ae55eeb888e8790e4478194a
cyw777 <20373375@buaa.edu.cn>
2023-12-16 19:27:50 +0800

    wo shi ni die

--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -1,10 +1,37 @@
 <template>
-  <div>deng lu ye mian</div>
+  <div>
+    <el-form :model="form" ref="form" class="demo-ruleForm">
+      <el-form-item label="用户名" prop="username">
+        <el-input v-model="form.username"></el-input>
+      </el-form-item>
+      <el-form-item label="密码" prop="password">
+        <el-input v-model="form.password"></el-input>
+      </el-form-item>
+      <el-form-item>
+        <el-button type="primary" @click="login">登录</el-button>
+      </el-form-item>
+    </el-form>
+  </div>
 </template>
 
 <script>
 export default {
-  name: "Login"
+  name: "Login",
+  data() {
+    return {
+      title: '登录',
+      is_login: true,
+      form: {
+        username: '',
+        password: '',
+      },
+    };
+  },
+  methods: {
+    login() {
+      console.log(this.form)
+    },
+  }
 }
 </script>
 
