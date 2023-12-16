02af4b876130aaaec1a1ff0ce929175f0b96dfd8
cyw777 <20373375@buaa.edu.cn>
2023-12-16 17:12:17 +0800

    wo shi ni die

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,10 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import HomeView from '../views/HomeView.vue'
+import LoginView from '../views/LoginView.vue'
+import UserRegisterView from '../views/UserRegisterView.vue'
+import ExpertRegisterView from '@/views/ExpertRegisterView';
+import ChatView from "@/views/ChatView";
 
 Vue.use(VueRouter)
 
@@ -19,11 +23,39 @@ const routes = [
     component: function () {
       return import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
     }
-  }
+  },
+  {
+    path: '/login',
+    name: 'login',
+    component: LoginView
+  },
+  {
+    path: '/userRegister',
+    name: 'userRegister',
+    component: UserRegisterView
+  },
+  {
+    path: '/expertRegister',
+    name: 'expertRegister',
+    component: ExpertRegisterView
+  },
+  {
+    path: '/chat/:chatID',
+    name: 'chat',
+    component: ChatView
+  },
 ]
 
 const router = new VueRouter({
+  mode: 'history',
+  base: process.env.BASE_URL,
   routes
 })
+router.beforeEach((to, from, next) => {
+  if (to.path === '/login'&&from.path!=='/register'&&from.path!=='/forget') {
+    localStorage.setItem("preRoute", router.currentRoute.fullPath);
+  }
 
+  next()
+})
 export default router
---
+++ src/views/LoginView.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>deng lu ye mian</div>
+</template>
+
+<script>
+export default {
+  name: "Login"
+}
+</script>
+
+<style scoped>
+
+</style>
