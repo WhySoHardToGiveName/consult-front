3378f1c5a7f0ce63350afb0289a50321ea17e722
陈百铭 <20373324@buaa.edu.cn>
2024-01-03 16:34:22 +0800

    111

--- src/views/ChatView.vue
+++ src/views/ChatView.vue
@@ -1,10 +1,88 @@
 <template>
-  <div>333</div>
+  <m-chat
+      ref="mChat"
+      :messages="messages"
+      @submit="submit"
+      :loadMore="loadMore"
+      height="100vh"
+  >
+  </m-chat>
 </template>
 
 <script>
+import MChat from "@maybecode/m-chat";
+
 export default {
-  name: "ChatView"
+  name: "ChatView",
+  components: {
+    MChat
+  },
+  data() {
+    return {
+      messages: [
+        {
+          id: 0, // 唯一id
+          name: "", // 姓名
+          avatar: "", // 头像地址
+          self: false, // 是否是自己
+          content: {
+            text: "123", // 文本
+            duration: "", // 时长
+            imageUrl: "", // 图片地址
+            videoUrl: "", // 视频地址
+            audioUrl: "", // 音频地址
+            fileUrl: "", // 文件地址
+            fileName: "", // 文件名称
+            fileSize: "", // 文件大小
+            filExt: "", // 文件扩展名
+          },
+          type: "text", // 文件类型:text|image|audio|video,
+          isBack: false,  // 控制该消息是否已经撤回,
+          time: '15:47' // 发送时间
+        },
+        {
+          id: 1, // 唯一id
+          name: "", // 姓名
+          avatar: "", // 头像地址
+          self: false, // 是否是自己
+          content: {
+            text: "123", // 文本
+            duration: "", // 时长
+            imageUrl: "", // 图片地址
+            videoUrl: "", // 视频地址
+            audioUrl: "", // 音频地址
+            fileUrl: "", // 文件地址
+            fileName: "", // 文件名称
+            fileSize: "", // 文件大小
+            filExt: "", // 文件扩展名
+          },
+          type: "text", // 文件类型:text|image|audio|video,
+          isBack: false,  // 控制该消息是否已经撤回,
+          time: '15:48' // 发送时间
+        },
+        {
+          id: 2, // 唯一id
+          name: "", // 姓名
+          avatar: "", // 头像地址
+          self: false, // 是否是自己
+          content: {
+            text: "123", // 文本
+            duration: "", // 时长
+            imageUrl: "", // 图片地址
+            videoUrl: "", // 视频地址
+            audioUrl: "", // 音频地址
+            fileUrl: "", // 文件地址
+            fileName: "", // 文件名称
+            fileSize: "", // 文件大小
+            filExt: "", // 文件扩展名
+          },
+          type: "text", // 文件类型:text|image|audio|video,
+          isBack: false,  // 控制该消息是否已经撤回,
+          time: '15:49' // 发送时间
+        }
+      ]
+    }
+  }
 }
 </script>
 
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,17 +1,59 @@
 <template>
-  <div class="home">
-    This is Home page
+  <div class="box-display">
+    <div class="person-box">
+      <el-popover placement="bottom"
+                  width="200"
+                  trigger="click">
+        <p>{{intro}}</p>
+        <div style="text-align: right; margin: 0">
+          <el-button size="mini" type="text" @click="visible = false">取消</el-button>
+          <el-button type="primary" size="mini" @click="visible = false">咨询</el-button>
+        </div>
+        <div class="person-card" slot="reference">
+          <img src="@/assets/hcm.png" alt="img">
+          <span style="margin-top: 5px;">胡春明</span>
+        </div>
+      </el-popover>
+    </div>
   </div>
+
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
 
 export default {
   name: 'HomeView',
   components: {
-    HelloWorld
+  },
+  data() {
+    return {
+      visible: false,
+      intro: '粉嫩小雪，寂寞学生妹，求求哥哥来草窝'
+    };
   }
 }
 </script>
+
+<style scoped>
+.box-display {
+  display: flex;
+  justify-content: center;
+  /*align-items: center;*/
+  height: 100vh;
+  width: 50vw;
+}
+.person-box {
+  position: center;
+  display: flex;
+
+}
+.person-box:hover{
+  cursor: pointer;
+}
+.person-card {
+  display: flex;
+  flex-direction: column;
+  width: 10vw;
+}
+</style>
